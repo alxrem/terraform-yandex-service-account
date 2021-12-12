559dd3d555b7000bb76307358655190ab90e5d10
Alexey Remizov <alexey@remizov.org>
2021-12-12 19:59:33 +0300

    Added descriptions of variables and outputs

--- variables.tf
+++ variables.tf
@@ -1,32 +1,44 @@
 variable "name" {
+  description = "Name of the service account"
+
   type = string
 }
 
 variable "description" {
+  description = "Description of the service account."
+
   type = string
 
   default = "Managed by terraform"
 }
 
-variable "roles" {
-  type = set(string)
-
-  default = []
-}
-
 variable "folder_id" {
+  description = "ID of the folder that the service account will be created in. Defaults to the provider folder configuration."
+
   type = string
 
   default = null
 }
 
+variable "roles" {
+  description = "Roles of the service account in the folder."
+
+  type = set(string)
+
+  default = []
+}
+
 variable "static_access_key" {
+  description = "Whether to create [service account static access key](https://cloud.yandex.com/docs/iam/operations/sa/create-access-key)."
+
   type = bool
 
   default = false
 }
 
 variable "service_account_key" {
+  description = "Whether to create [service account authorized key](https://cloud.yandex.com/docs/iam/concepts/authorization/key)."
+
   type = bool
 
   default = false
