1cab51dc1a3c2ed260236acf9d4c8a8d5a1da170
Alexey Remizov <alexey@remizov.org>
2024-09-07 00:43:52 +0300

    Extended configuration of keys

--- variables.tf
+++ variables.tf
@@ -1,3 +1,17 @@
+# Copyright 2021-2024 Alexey Remizov <alexey@remizov.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 variable "name" {
   description = "Name of the service account"
 
@@ -28,18 +42,44 @@ variable "roles" {
   default = []
 }
 
-variable "static_access_key" {
-  description = "Whether to create [service account static access key](https://cloud.yandex.com/docs/iam/operations/sa/create-access-key)."
+variable "static_access_keys" {
+  description = <<DESCRIPTION
+  [Static access keys](https://cloud.yandex.com/docs/iam/operations/sa/create-access-key) of account.
 
-  type = bool
+  Map from the symbolic name to properties of the keys.
+  DESCRIPTION
 
-  default = false
+  type = map(object({
+    description = optional(string, "Managed by Terraform")
+    pgp_key     = optional(string)
+
+    output_to_lockbox = optional(object({
+      secret_id            = string
+      entry_for_secret_key = string
+    }))
+  }))
+
+  default = {}
 }
 
-variable "service_account_key" {
-  description = "Whether to create [service account authorized key](https://cloud.yandex.com/docs/iam/concepts/authorization/key)."
+variable "service_account_keys" {
+  description = <<DESCRIPTION
+  [Authorized keys](https://cloud.yandex.com/docs/iam/concepts/authorization/key) of account.
 
-  type = bool
+  Map from the symbolic name to properties of the keys.
+  DESCRIPTION
 
-  default = false
+  type = map(object({
+    description   = optional(string, "Managed by Terraform")
+    format        = optional(string)
+    key_algorithm = optional(string)
+    pgp_key       = optional(string)
+
+    output_to_lockbox = optional(object({
+      secret_id             = string
+      entry_for_private_key = string
+    }))
+  }))
+
+  default = {}
 }
